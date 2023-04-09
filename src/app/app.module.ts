668a25d21fcd29bbe26921ddf57d6d70b0c6758c
limitedbug <mrbugexe@gmail.com>
2023-04-08 23:56:31 -0600

    adicion de materials y estructura principal
    se añadio el materials de angular para funcionamiento del proyecto y se realizo la estructura base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NewTaskComponent } from './components/forms/new-task/new-task.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NewTaskComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
